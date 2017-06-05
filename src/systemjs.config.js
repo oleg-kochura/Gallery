1ac53ae7b1c8e4112753fbaf6ed9d14d4a08e7ec
oleg.kochura <o.kochura@viseven.com>
2017-06-05 23:49:17 +0300

    deploy

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -35,6 +35,7 @@
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
     },
+    baseURL: '/Gallery/',
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
